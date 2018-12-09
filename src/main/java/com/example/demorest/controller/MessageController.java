e2a9c4f4a900e3539d7cd77de2c848bccebec0b3
Vanessa M. Paco Ramos <vanessapr.85@gmail.com>
2018-12-09 09:29:54 -0500

    added message post method

--- src/main/java/com/example/demorest/controller/MessageController.java
+++ src/main/java/com/example/demorest/controller/MessageController.java
@@ -1,16 +1,21 @@
 package com.example.demorest.controller;
 
 import com.example.demorest.model.Message;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class MessageController {
 
-    @ResponseBody
     @GetMapping("/message")
     Message send() {
         return new Message("First message");
+    }
+
+    @PostMapping("/message")
+    Message echo(@RequestBody Message message) {
+        return message;
     }
 }
